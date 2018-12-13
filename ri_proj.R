e0cb0786328fa9d2384cf26cab30588a8e5fdac7
adamwsmith <45830713+adamwsmith@users.noreply.github.com>
2018-12-13 00:37:40 -0800

    Update ri_proj.R

--- ri_proj.R
+++ ri_proj.R
@@ -7,26 +7,38 @@ eladat=read.csv("psat-ela-school Quick Report.csv")
 colnames(mathdat)=c("dist","school","grade","num_enrolled","pctested","not","partial","meeting","exceed","meetandexceed","avgscalescore")
 colnames(eladat)=c("dist","school","grade","num_enrolled","pctested","not","partial","meeting","exceed","meetandexceed","avgscalescore")
 
-
+data=mathdat
 
 #fucntion to remove header, extra cells,& non/excluded data (* fields)
 cleaner=function(data){
-  data=data[-1,]
-  for(r in 3:length(colnames(mathdat))){ #ensure fields are numeric NOT factor
-    mathdat[,r]=as.numeric(mathdat[,r])
+  for(c in 1:2){ #convert character columns to char
+    data[,c]=as.character(data[,c])
   }
-  # exclude=numeric()
-  # for(i in 2:length(data[,1])){ #identify/remove lines with *
-  #   if(is.na(data[i,][4]%%1)){
-  #     exclude=c(exclude,i)
-  #   }
-  # }
-  #data=data[-exclude,]
+  exclude=numeric()
+  for(r in 1:length(data[,1])){
+    for(c in 3:length(colnames(data))){
+      if(data[r,c][1]=="*" | data[r,c][1]=="**" | data[r,c][1]=="***"){
+        exclude=c(exclude,r)
+      } else {
+        data[r,c]=as.numeric(data[r,c][1])
+      }
+    }
+  }
+    exclude=unique(exclude)
+    data=data[-c(1,exclude),]
+    return(data)
 }
+  
 
 cleaner(mathdat)
 
+unique(exclude)
 data=mathdat
+length(data[,1])
+
+data[,1]=as.character(data[,1])
+data[,1]
+data[10,4]
 
 mathdat=mathdat[-1,]
 mathdat[,4]=as.numeric(mathdat[,4])
